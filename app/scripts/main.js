6732eecddcd9478d8bacdd169ec94e9b4eba9175
td0 <tadho@me.com>
2017-04-19 19:11:21 +0700

    js cache updater
    Signed-off-by: td0 <tadho@me.com>

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,6 +3,22 @@ window.onload = function(){
   $('.init-spinner').removeClass('is-active');
 }
 
+// Check if a new cache is available on page load.
+window.addEventListener('load', function(e) {
+
+  window.applicationCache.addEventListener('updateready', function(e) {
+    if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+      // Browser downloaded a new app cache.
+      if (confirm('A new version of this site is available. Load it?')) {
+        window.location.reload();
+      }
+    } else {
+      // Manifest didn't changed. Nothing new to server.
+    }
+  }, false);
+
+}, false);
+
 // ====================================================================================
 
 var playerDB, scoreDB, turn;
